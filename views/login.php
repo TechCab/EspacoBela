ec11236fdc082b439a4ee4531e3f171086fcd4be
lair_rezende <lair_rezende@hotmail.com>
2021-05-11 21:19:55 -0300

    login.php

---
+++ views/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet"  href="estilo1.css">
+</head>
+<body>
+    <div id="corpo-form">
+<h1>Entre para Agendar</h1>    
+<form method = "POST">
+    <input type="email" name="email_cad" required="required"  placeholder="E-mail"> 
+    <input type="senha"  name="senha_cad" required="required" placeholder="Senha">
+    <input type="submit" value="entrar"> 
+    <a href="cadastro.php"> Novo por aqui? <strong>Cadastre-se</a>
+    <a href=""> Esqueceu senha? <strong>Recupere</a>
+</form>
+</div>
+   
+</body>
+</html>
