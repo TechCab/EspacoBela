2f1552cfc26561a68a618c63dae3ab5577039e2d
Tcarauba <carauba.guitar@gmail.com>
2021-05-14 18:53:29 -0300

    contato.php

---
+++ views/contato.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contato</title>
+    <link rel="stylesheet"  href="estilo1.css">
+</head>
+<body>
+    <div id="corpo-form">
+	<h1>Fale Conosco<h1>    
+	<form method = "POST">
+    	<input type="text" name="nome_cad" required="required"  placeholder="Nome Completo"/>
+    	<input type="email" name="email_cad" required="required"  placeholder="E-mail"/> 
+    	<input type="mensagem"  name="senha_cad" required="required" placeholder="mensagem"/>
+    	<input type="submit" value="Enviar"/> 
+    </form>
+	</div>
+
+	<footer class="footer-distributed">
+		<div class="footer-left">
+			<h3>Espaço<span>Bela</span></h3>
+				<p class="footer-links">
+					<a class="link-1" href="index.html">Inicio</a>
+					<a href="cadastro.php">Cadastrar</a>
+					<a href="login.php">Agendar</a>	
+					<a href="contato.php">Contato</a>
+				</p>
+				<p class="footer-company-name">Espaço Bela © 2021</p>
+		</div>
+		<div class="footer-right">
+				<p class="footer-company-about">
+					<span>Sobre nós</span>
+					Equipe: Lair, Raquel, Thiago, Maria Clara
+				</p>
+		</div>
+</body>
+</html>
