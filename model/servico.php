48e51991afa9c624e921a48988eb910847240a57
RRRAMOS <dsraquelramos@gmail.com>
2021-05-19 19:36:01 -0300

    atributos, getter, setter

---
+++ model/servico.php
@@ -0,0 +1,42 @@
+<?php
+class servico {
+
+    private $id;
+    private $preco;
+    private $descricao;
+    private $tempo;
+}
+public function setID($id)
+{
+    $this-> id= $id;
+}
+public function getID()
+{
+    return $this->id;
+}
+public function setPreco($preco)
+{
+    $this-> preco= $preco;
+}
+public function getPreco()
+{
+    return $this->preco;
+}
+public function setDescricao($descricao)
+{
+    $this-> descricao= $descricao;
+}
+public function getDescricao()
+{
+    return $this->descricao;
+}
+public function setTempo($tempo)
+{
+    $this-> tempo= $tempo;
+}
+public function getTempo()
+{
+    return $this->tempo;
+}
+
+?>
