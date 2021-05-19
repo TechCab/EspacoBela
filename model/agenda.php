1c602d9ccfb778b7f0faf2ea2bc151eb8bcf33ef
MariaxClara <m.clara.almeida@hotmail.com>
2021-05-19 19:37:08 -0300

    atributos, getters, setters

---
+++ model/agenda.php
@@ -0,0 +1,44 @@
+<?php
+
+class Servico {
+    private $id;
+    private $hora;
+    private $data;
+    private $status;
+    private $servico_id;
+    private $cliente_id;
+}
+public function setID($id)
+{
+    $this->id = $id;
+}
+public function getID()
+{
+    return $this->id;
+}
+public function setHora($hora)
+{
+    $this->hora = $hora;
+}
+public function getHora()
+{
+    return $this->hora;
+}
+public function setData($data)
+{
+    $this->data = $data;
+}
+public function getData()
+{
+    return $this->data;
+}
+public function setStatus($status)
+{
+    $this->status = $status;
+}
+public function getStatus()
+{
+    return $this->status;
+}
+
+?>
