44a030934b36300d815a9d1a183a67503963525e
MariaxClara <m.clara.almeida@hotmail.com>
2021-05-23 19:53:17 -0300

    tela para usuario fazer agendamento

---
+++ views/agendamento.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agendamento</title>
+    <link rel="stylesheet"  href="estilo1.css">
+</head>
+<body>
+    <div id="agenda-form">
+<h1>Agende seu serviço</h1>    
+<form method = "POST">
+    <p>Escolha sua data preferida:</p>
+    <input type="date" name="dataAgenda_client" required="required"  placeholder="Dia do agendamento"> 
+	<p>Qual serviço você quer?</p>
+	<select name="servico">
+		<option>Manicure</option>
+		<option>Pedicure</option> 
+		<option>Escova</option>
+		<option>Depilação</option>
+        <option>Sobrancelha</option>
+        <option>Maquiagem</option>
+        <option>Progressiva</option>
+        <option>Alongamento</option>
+        <option>Luzes</option>
+	</select><br />
+  <input type="submit" value="Agendar"> 
+    <a href="index.html"> Voltar para tela inicial <strong>aqui</a>
+</form>
+</div>
+</body>
+</html>
