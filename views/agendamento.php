9c99c0fe46c338882a6d5eb59dd5137de132733b
RRRAMOS <dsraquelramos@gmail.com>
2021-06-06 20:13:39 -0300

    revisão rota e id

--- views/agendamento.php
+++ views/agendamento.php
@@ -5,10 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agendamento</title>
-    <link rel="stylesheet"  href="./css/estilo1.css">
+    <link rel="stylesheet"  href="../css/estilo1.css">
 </head>
 <body>
-    <div id="agenda-form">
+    <div id="corpo-form">
 <h1>Agende seu serviço</h1>    
 <form method = "POST">
     <p>Escolha sua data preferida:</p>
