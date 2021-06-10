de418a86cf4265697517545a3427fef337053b7c
Tcarauba <carauba.guitar@gmail.com>
2021-06-10 18:45:12 -0300

    Atualização Tela cadastro usuário

--- views/cadastroUsuario.php
+++ views/cadastroUsuario.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro</title>
-    <link rel="stylesheet"  href="../views/estilo1.css">
+    <title>Cadastro Usuário</title>
+    <link rel="stylesheet"  href="../css/estilo1.css">
 </head>
 <body>
     <div id="corpo-form">
