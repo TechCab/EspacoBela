ea8682bd640455b99cd19368b81cf9fa8b7b6baa
RRRAMOS <dsraquelramos@gmail.com>
2021-06-11 22:35:18 -0300

    interface detalhes agendamento

---
+++ views/confirmaAgendamento.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <title>confirmação agendamento</title>
+    <link rel="stylesheet"  href="../css/estilo1.css">
+</head>
+<body>
+    <div id="corpo-form">
+  <h1>confirmação de Agendamento</h1>  
+
+  <div class="container">
+       
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>Firstname</th>
+          <th>Lastname</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+
+    </table>
+  </div>
+
+</body>
+</html>
