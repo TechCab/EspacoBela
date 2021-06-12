7d84fce47b42fb98a93d9f310c98c8033f8ab6ad
RRRAMOS <dsraquelramos@gmail.com>
2021-06-11 22:34:12 -0300

    inclusão menu opções adm

--- views/controleAdm.php
+++ views/controleAdm.php
@@ -4,12 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login Administrador</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet"  href="../css/estilo1.css">
+    <title>login Administrador</title>
 </head>
 <body>
     <div id="corpo-form">
-<h1>Controle Administrador</h1>  
+    <h1>Controle Administrador</h1>  
 
+    <div class="container">
+        <ul class="nav nav-tabs">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Disponibilizar Agenda</a></li>
+        <li><a href="#">Ver CLientes</a></li>
+        <li><a href="#">Ver Administradores</a></li>
+        <li><a href="#">Ver Agendamentos </a></li>
+    </ul>
+    <br>
+    <p> Selecione a função desejada
+</div>
 </body>
 </html>
