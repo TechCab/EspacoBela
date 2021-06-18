23346a576c584723b8eacecc56a376d7e9f8dd28
RRRAMOS <dsraquelramos@gmail.com>
2021-06-17 22:53:58 -0300

    inclusão view para listar agendamentos

---
+++ views/listaAgendamentos.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet"  href="../css/estilo1.css">
+    <title>Lista de Agendamentos</title>
+</head>
+<body>
+  <div id="corpo-form">
+        <h1>Agendamentos Confirmados</h1>
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Data</th>
+          <th>Hora</th>
+          <th>Serviço</th>
+          <th>Cliente</th>
+        </tr>
+      </thead>
+    </table>
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+  
+
+    <a href="../index.html"> Voltar para tela inicial</a>
+</body>
+</html>
